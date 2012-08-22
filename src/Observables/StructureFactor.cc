30adfc5caf298f862a0780ae5730919c0bdc76f4
Ethan Brown <ethan.w.brown@gmail.com>
2012-08-21 21:48:02 -0700

    fixed structurefactor for multiple processors

--- src/Observables/StructureFactor.cc
+++ src/Observables/StructureFactor.cc
@@ -267,53 +267,53 @@ void StructureFactorClass::Accumulate()
         rho_k_real(ki)+=a;
         rho_k_imag(ki)+=b;
       }
-    } else {
-      if (!PathData.Path.LongRange) {
+    }
+  } else {
+    if (!PathData.Path.LongRange) {
+      for (int slice=0; slice < PathData.NumTimeSlices()-1; slice++)
+        PathData.Path.CalcRho_ks_Fast(slice, Species1);
+      if (Species2 != Species1)
         for (int slice=0; slice < PathData.NumTimeSlices()-1; slice++)
-          PathData.Path.CalcRho_ks_Fast(slice, Species1);
-        if (Species2 != Species1)
-          for (int slice=0; slice < PathData.NumTimeSlices()-1; slice++)
-            PathData.Path.CalcRho_ks_Fast(slice, Species2);
-      }
-      if (Additionalkvecs.extent(0)!=0){
-        assert(Species1==Species2);
-        for (int slice=0;slice<PathData.NumTimeSlices()-1;slice++)
-          PathData.Path.CalcRho_ks_Slow(slice,Species1,Additionalkvecs,AdditionalRho_k);
-      }
-      for (int slice=0;slice<PathData.NumTimeSlices()-1;slice++) {
-        multimap<double,double > kList;
-        for (int ki=0; ki<kVecs.size(); ki++) {
-          double a = PathData.Path.Rho_k(slice, Species1, ki).real();
-          double b = PathData.Path.Rho_k(slice, Species1, ki).imag();
-          double c = PathData.Path.Rho_k(slice, Species2, ki).real();
-          double d = PathData.Path.Rho_k(slice, Species2, ki).imag();
-          // \f$ Sk(ki) :=  Sk(ki) + \Re(rho^1_k * rho^2_{-k}) \f
-          double sk=a*c+b*d;
-          rho_k_real(ki)+=a;
-          rho_k_imag(ki)+=b;
-          if (sk>SkMax){
-           SkMax=sk;
-           MaxkVec=kVecs(ki);
-          }
-          double kMag=sqrt(kVecs(ki)[0]*kVecs(ki)[0]+kVecs(ki)[1]*kVecs(ki)[1]);
-          kList.insert(pair<double,double> (kMag,sk));
-                      //      cerr<<slice<<" "<<ki<<" "<<sk<<endl;
-          Sk(ki) += sk;
+          PathData.Path.CalcRho_ks_Fast(slice, Species2);
+    }
+    if (Additionalkvecs.extent(0)!=0){
+      assert(Species1==Species2);
+      for (int slice=0;slice<PathData.NumTimeSlices()-1;slice++)
+        PathData.Path.CalcRho_ks_Slow(slice,Species1,Additionalkvecs,AdditionalRho_k);
+    }
+    for (int slice=0;slice<PathData.NumTimeSlices()-1;slice++) {
+      multimap<double,double > kList;
+      for (int ki=0; ki<kVecs.size(); ki++) {
+        double a = PathData.Path.Rho_k(slice, Species1, ki).real();
+        double b = PathData.Path.Rho_k(slice, Species1, ki).imag();
+        double c = PathData.Path.Rho_k(slice, Species2, ki).real();
+        double d = PathData.Path.Rho_k(slice, Species2, ki).imag();
+        // \f$ Sk(ki) :=  Sk(ki) + \Re(rho^1_k * rho^2_{-k}) \f
+        double sk=a*c+b*d;
+        rho_k_real(ki)+=a;
+        rho_k_imag(ki)+=b;
+        if (sk>SkMax){
+         SkMax=sk;
+         MaxkVec=kVecs(ki);
         }
-        for (int ki=kVecs.size();ki<kVecs.size()+Additionalkvecs.size();ki++){
-          int kk=ki-kVecs.size();
-          double a = (AdditionalRho_k(slice, Species1, kk)).real();
-          double b = AdditionalRho_k(slice, Species1, kk).imag();
-          double c = AdditionalRho_k(slice, Species2, kk).real();
-          double d = AdditionalRho_k(slice, Species2, kk).imag();
+        double kMag=sqrt(kVecs(ki)[0]*kVecs(ki)[0]+kVecs(ki)[1]*kVecs(ki)[1]);
+        kList.insert(pair<double,double> (kMag,sk));
+                    //      cerr<<slice<<" "<<ki<<" "<<sk<<endl;
+        Sk(ki) += sk;
+      }
+      for (int ki=kVecs.size();ki<kVecs.size()+Additionalkvecs.size();ki++){
+        int kk=ki-kVecs.size();
+        double a = (AdditionalRho_k(slice, Species1, kk)).real();
+        double b = AdditionalRho_k(slice, Species1, kk).imag();
+        double c = AdditionalRho_k(slice, Species2, kk).real();
+        double d = AdditionalRho_k(slice, Species2, kk).imag();
 
-          // \f$ Sk(ki) :=  Sk(ki) + \Re(rho^1_k * rho^2_{-k}) \f
-          double sk=a*c+b*d;
-          Sk(ki) += sk;
-          rho_k_real(ki)+=a;
-          rho_k_imag(ki)+=b;
+        // \f$ Sk(ki) :=  Sk(ki) + \Re(rho^1_k * rho^2_{-k}) \f
+        double sk=a*c+b*d;
+        Sk(ki) += sk;
+        rho_k_real(ki)+=a;
+        rho_k_imag(ki)+=b;
 
-        }
       }
     }
   }
