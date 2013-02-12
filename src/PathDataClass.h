5cb1e101951ce5a5103fac02386806e28df1da73
Ethan Brown <ethan.w.brown@gmail.com>
2013-02-12 15:31:58 -0800

    updated copyright message to include myself and new website

--- src/PathDataClass.h
+++ src/PathDataClass.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////
-// Copyright (C) 2003-2006 Bryan Clark and Kenneth Esler   //
+// Copyright (C) 2003-2013  B. Clark, K. Esler, E. Brown   //
 //                                                         //
 // This program is free software; you can redistribute it  //
 // and/or modify it under the terms of the GNU General     //
@@ -8,10 +8,10 @@
 // (at your option) any later version.  This program is    //
 // distributed in the hope that it will be useful, but     //
 // WITHOUT ANY WARRANTY; without even the implied warranty //
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. //  
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. //
 // See the GNU General Public License for more details.    //
 // For more information, please see the PIMC++ Home Page:  //
-//           http://pathintegrals.info                     //
+// http://code.google.com/p/pimcplusplus/                  //
 /////////////////////////////////////////////////////////////
 
 #ifndef PATH_DATA_CLASS
